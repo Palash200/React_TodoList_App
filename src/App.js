26850f6d71f3878338c604d5ba8e924df890ee7d
Palash200 <85168212+Palash200@users.noreply.github.com>
2022-09-13 11:27:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  let initTodo;
+  // If todos List is Empty
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  // Functionality of delete
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  // Add a todo
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+  //declaring the todos list
+  const [todos, setTodos] = useState(initTodo);
+  // useEffect retains the todos list after reloading the page
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  // Used to switch between navs without switching time
+  return ( 
+    <> 
+    <Router>
+      <Header title="My Todos List" searchBar={false} /> 
+      <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} /> 
+            </>)
+          }}> 
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route> 
+        </Switch> 
+      <Footer />
+    </Router>
+    </>
+  );
+}
+
+export default App;
